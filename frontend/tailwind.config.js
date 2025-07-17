8baf2718393300ca059a5ce4e97718c552a6bd68
JoaoBeltrami <jon_blt@hotmail.com>
2025-07-17 15:12:52 -0300

    Correção geral

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 export default {
-  darkMode: 'class', // ativa o dark mode via classe 'dark'
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,6 +9,10 @@ export default {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        cinzel: ['Cinzel', 'serif'],
+        fira: ['Fira Sans', 'sans-serif'],
+        patrick: ['Patrick Hand', 'cursive'],
+        orbitron: ['Orbitron', 'sans-serif'],
       },
       animation: {
         'gradient-x': 'gradient-x 8s ease infinite',
